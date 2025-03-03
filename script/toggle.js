0d861674a7f627045117efc25cc8f13c9bb2d940
TUMIT_HASAN <tumithasan1@gmail.com>
2025-03-03 21:29:55 +0600

    fix add money and cash out issue

--- script/toggle.js
+++ script/toggle.js
@@ -1,9 +1,10 @@
 document.getElementById("cashout").style.display = "none"
+document.getElementById("addMoney").style.display = "none"
 
 document.getElementById("cashout-box").addEventListener("click", function () {
     document.getElementById("cashout").style.display = "block";
     document.getElementById("addMoney").style.display = "none";
-    
+
 })
 document.getElementById("add-money-box").addEventListener("click", function () {
     document.getElementById("addMoney").style.display = "block";
