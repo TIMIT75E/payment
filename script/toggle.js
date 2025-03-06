0f9e5e94487e1b3672ec7e902aa5dc5e6e420b47
TUMIT_HASAN <tumithasan1@gmail.com>
2025-03-06 10:34:14 +0600

    fix some bug in toggle file

--- script/toggle.js
+++ script/toggle.js
@@ -8,17 +8,20 @@ document.getElementById("cashout-box").addEventListener("click", function () {
     document.getElementById("cashout").style.display = "block";
     document.getElementById("addMoney").style.display = "none";
     document.getElementById("transfer-mony").style.display = "none";
+    document.getElementById("Pay-Bill").style.display = "none";
 
 })
 document.getElementById("add-money-box").addEventListener("click", function () {
     document.getElementById("addMoney").style.display = "block";
     document.getElementById("cashout").style.display = "none";
     document.getElementById("transfer-mony").style.display = "none";
+    document.getElementById("Pay-Bill").style.display = "none";
 })
 document.getElementById("transfer-box").addEventListener("click", function () {
     document.getElementById("transfer-mony").style.display = "block";
     document.getElementById("addMoney").style.display = "none";
     document.getElementById("cashout").style.display = "none";
+    document.getElementById("Pay-Bill").style.display = "none";
 })
 document.getElementById("pay-bill-box").addEventListener("click", function () {
     document.getElementById("Pay-Bill").style.display = "block";
