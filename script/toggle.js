62e6f746f802a335e2777795a0cdbe48cd15e17e
TUMIT_HASAN <tumithasan1@gmail.com>
2025-03-06 10:30:27 +0600

    compelete the pay bill section

--- script/toggle.js
+++ script/toggle.js
@@ -1,6 +1,8 @@
 document.getElementById("cashout").style.display = "none";
 document.getElementById("addMoney").style.display = "none";
 document.getElementById("transfer-mony").style.display = "none";
+document.getElementById("Pay-Bill").style.display = "none";
+
 
 document.getElementById("cashout-box").addEventListener("click", function () {
     document.getElementById("cashout").style.display = "block";
@@ -16,5 +18,11 @@ document.getElementById("add-money-box").addEventListener("click", function () {
 document.getElementById("transfer-box").addEventListener("click", function () {
     document.getElementById("transfer-mony").style.display = "block";
     document.getElementById("addMoney").style.display = "none";
-    document.getElementById("cashout").style.display = "none"; 
+    document.getElementById("cashout").style.display = "none";
+})
+document.getElementById("pay-bill-box").addEventListener("click", function () {
+    document.getElementById("Pay-Bill").style.display = "block";
+    document.getElementById("transfer-mony").style.display = "none";
+    document.getElementById("addMoney").style.display = "none";
+    document.getElementById("cashout").style.display = "none";
 })
