042a1e4d1ef85b8758850f5f1c0592231f9882d4
TUMIT_HASAN <tumithasan1@gmail.com>
2025-03-06 10:21:18 +0600

    compelete the transfer money section

--- script/toggle.js
+++ script/toggle.js
@@ -1,12 +1,20 @@
-document.getElementById("cashout").style.display = "none"
-document.getElementById("addMoney").style.display = "none"
+document.getElementById("cashout").style.display = "none";
+document.getElementById("addMoney").style.display = "none";
+document.getElementById("transfer-mony").style.display = "none";
 
 document.getElementById("cashout-box").addEventListener("click", function () {
     document.getElementById("cashout").style.display = "block";
     document.getElementById("addMoney").style.display = "none";
+    document.getElementById("transfer-mony").style.display = "none";
 
 })
 document.getElementById("add-money-box").addEventListener("click", function () {
     document.getElementById("addMoney").style.display = "block";
     document.getElementById("cashout").style.display = "none";
+    document.getElementById("transfer-mony").style.display = "none";
+})
+document.getElementById("transfer-box").addEventListener("click", function () {
+    document.getElementById("transfer-mony").style.display = "block";
+    document.getElementById("addMoney").style.display = "none";
+    document.getElementById("cashout").style.display = "none"; 
 })
